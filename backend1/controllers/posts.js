4d5f1b4edf748c61c519d00c62bc6e856b0e3908
Louis <louis.m.newton@hotmail.fr>
2021-07-27 15:35:46 +0200

    controllers newPost

--- backend1/controllers/posts.js
+++ backend1/controllers/posts.js
@@ -1,6 +1,36 @@
+  
+// const db = require('../database');
+
 // Création d'un post
 exports.newPost = (req,res,next) => {
-    res.status(200).json({ message: 'new post' });
+    const date = new Date();
+    let min = date.getMinutes(); 
+    let hour = date.getHours();
+    let day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    let month = (date.getMonth()+1) < 10 ? "0" +(date.getMonth()+1) : (date.getMonth()+1);
+    let year = date.getFullYear();
+    let image = "";
+
+    // S'il y a image, on l'enregistre avec le protocol http/ + localhst + dans le dossier image + le nom du fichier
+    if (req.file) { image = `${req.protocol}://${req.get("host")}/images/${req.file.filename}`}
+
+    const post = {
+        id_user : req.id_user,
+        content : req.body.content,
+        image : image,
+        date : year + "-" + month + "-" + day + "-" + hour + "-" + min,
+        status : 1
+    };
+
+    console.log(post);
+
+    // Injection du post dans la table post dans la BDD
+    // db.query('INSERT INTO post SET ?', post, (err, result) => {
+    //     if (err) return res.status(400).json({error : err});
+    //     return res.status(201).json({ message : "post enregistré dans la base de donnée"})
+    // });
+
+    res.status(201).json({ message: 'new post' });
 }
 
 exports.getAllPosts = (req,res,next) => {
